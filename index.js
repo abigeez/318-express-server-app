8365f25b6d5460a5b0a0402bfac99551e78f2ac1
Abi Garner <abigail.gee@hotmail.com>
2024-05-29 23:11:24 -0400

    trying to debug why server wouldn't work

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const http = require("http");
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type, text/html");
-    res.write('Hello World');
-    res.end();
+const
 
-});
-server.listen(3000,'localhost', () => {
-    console.log('listening for request on port 3000');
-});
+
+
+
+
+// const http = require('http');
+// const hostname = '127.0.01';
+// const port=3000;
+
+// const server = http.createServer((req,res)=> {
+// res.statusCode = 200;
+// res.setHeader('Content-Type','text/html');
+// res.end('Hello World!');
+
+// });
+
+// server.listen(port,hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
