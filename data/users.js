dbf0a863d186ad89330b23902e0d19aa6054a356
Abi Garner <abigail.gee@hotmail.com>
2024-05-30 01:45:13 -0400

    adding random data from data generator

---
+++ data/users.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+const users = [
+    {
+    id: 1,
+    username: "jmaypowder0",
+    name: "Juieta",
+    email: "jgravener0@unc.edu"
+  }, 
+  {
+    id: 2,
+    username: "vdocket1",
+    name: "Van",
+    email: "vfermer1@independent.co.uk"
+  },
+   {
+    id: 3,
+    username: "egresty2",
+    name: "Elysee",
+    email: "ebracher2@people.com.cn"
+  },
+    {
+    id: 4,
+    username: "cblayney3",
+    name: "Corbett",
+    email: "csowter3@bluehost.com"
+  }
+];
+module.exports = users;
