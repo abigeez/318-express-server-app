adf0a61bf9fa3fbbab3883344c1213bcc0f470c6
Abi Garner <abigail.gee@hotmail.com>
2024-05-29 22:09:02 -0400

    created package.json

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require("http");
-const server=http.createServer((req,res) => {
+const server = http.createServer((req,res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type, text/html");
     res.write('Hello World');
     res.end();
 
 });
-server.listen(3000,'local host', () => {
+server.listen(port: 3000,'localhost', () => {
     console.log('listening for request on port 3000');
 });
