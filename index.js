1b0c0e577f6ed04fa93167cadd0e0d4a9ec31eab
Abi Garner <abigail.gee@hotmail.com>
2024-05-30 00:59:44 -0400

    added express and some request methods

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const
+const express = require("express");
+const app = express();
+const port = 5050;
+
+app.listen(port,() => {
+
+    console.log(`Server listening on port:${port}.`);
+
+})
 
 
+app.get("/",(req,res) => {
+res.send("Welcome to the first page!");
+});
 
-
-
-// const http = require('http');
-// const hostname = '127.0.01';
-// const port=3000;
-
-// const server = http.createServer((req,res)=> {
-// res.statusCode = 200;
-// res.setHeader('Content-Type','text/html');
-// res.end('Hello World!');
-
-// });
-
-// server.listen(port,hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
+app.get("/about",(req,res) => {
+ res.send("About me");
+    });
