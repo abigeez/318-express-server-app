4fb23ceb9e52635da1e27185c0714602c02c70a8
Abi Garner <abigail.gee@hotmail.com>
2024-05-30 02:02:17 -0400

    exporting modules

--- data/posts.js
+++ data/posts.js
@@ -1,1 +1,23 @@
-const posts = 
+const posts = [{
+    id: 1,
+    userId: 1,
+    title: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus."
+}, {
+    id: 2,
+    userId: 2,
+    title: "Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+    content: "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus."
+}, {
+    id: 3,
+    userId: 3,
+    title: "Quisque ut erat.",
+    content: "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede."
+}, {
+    id: 4,
+    userId: 4,
+    title: "Mauris sit amet eros.",
+    content: "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem."
+}];
+
+module.exports = posts;
