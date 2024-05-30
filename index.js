65374b8070d31e2c78fb8cda245130a961e9565f
Abi Garner <abigail.gee@hotmail.com>
2024-05-29 21:57:15 -0400

    adding listener

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const http = require("http");
-
-.createServer((req,res) => {
+const server=http.createServer((req,res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type, text/html");
     res.write('Hello World');
     res.end();
 
-}).listen(3000, () => {
-    console.log('server GOOD')
-}
-)
+});
+server.listen(3000,'local host', () => {
+    console.log('listening for request on port 3000');
+});
