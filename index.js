03b220b47cf4d5f1f8b6a12d627b445b708b1916
Abi Garner <abigail.gee@hotmail.com>
2024-05-31 01:47:44 -0400

    added view engines to pages

--- index.js
+++ index.js
@@ -13,17 +13,30 @@ const posts = require('./data/posts.js');
 app.listen(port,() => {
 
     console.log(`Server listening on port:${port}.`);
-//get route for the html home page
-});
-app.get('/',(req,res)=> {
-    res.sendFile('./views/homepage.html',{root: __dirname});
-})
 
+});
+//get route for html home page//
+
+// app.get('/',(req,res)=> {
+//     res.sendFile('./views/homepage.html',{root: __dirname});
+// })
 
 // get route for the html form, login page
-app.get('/login',(req,res) => {
-    res.sendFile('./views/index.html',{root: __dirname});
+// app.get('/login',(req,res) => {
+//     res.sendFile('./views/index.html',{root: __dirname});
+// });
+
+//establishing route using view engine for homepage///
+app.get('/',(req,res) => {
+    res.render('index');
 });
+
+// route using view engine for login page//
+
+app.get('/login',(req,res) => {
+    res.render('login');
+})
+
 
 //redirect for homepage back to root
 app.get('/homepage', (req,res) => {
